2325d5f045a1b8f68774257209f32e38a33a5896
cem-cvs <cemcvs@gmail.com>
2025-03-15 17:40:09 +0200

    Update base.py

--- models/base.py
+++ models/base.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://famagustadb_owner:npg_zeMv72jOIgSy@ep-muddy-brook-abntf4h4-pooler.eu-west-2.aws.neon.tech/famagusa_db?sslmode=require"
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://famagustadb_owner:npg_zeMv72jOIgSy@ep-muddy-brook-abntf4h4-pooler.eu-west-2.aws.neon.tech/famagusa_db?sslmode=require")
 
 # Create Database Engine
 engine = create_engine(DATABASE_URL)
