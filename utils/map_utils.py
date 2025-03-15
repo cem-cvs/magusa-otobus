ef986a93b332587358e213a18de92ddc2aa86a7f
cem-cvs <cemcvs@gmail.com>
2025-03-15 07:43:29 +0200

    Update map_utils.py

--- utils/map_utils.py
+++ utils/map_utils.py
@@ -11,3 +11,11 @@ def add_landmark_markers(map_obj, landmarks):
             popup=landmark["name"],
             tooltip=landmark["name"]
         ).add_to(map_obj)
+
+def filter_landmarks(landmarks, search_term):
+    """Filter landmarks based on a search term."""
+    return [
+        landmark for landmark in landmarks
+        if search_term.lower() in landmark["name"].lower()
+        or search_term.lower() in landmark["description"].lower()
+    ]
